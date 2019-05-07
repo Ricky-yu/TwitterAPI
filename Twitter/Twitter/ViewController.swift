4c58fc4fe481601a3e4b7b208df632bb8ddf73fc
aucfan-chen <sinyu.chen@aucfan.com>
2019-05-07 23:56:49 +0900

    create new project

---
+++ Twitter/Twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by CHEN SINYU on 2019/05/07.
+//  Copyright © 2019 CHEN SINYU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
